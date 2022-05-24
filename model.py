7d69c5d1d192fc988291525656fdb60e910c9fef
ozgraslan <ozgraslan17@gmail.com>
2022-05-24 16:47:26 +0300

    Added discriminator model.

--- model.py
+++ model.py
@@ -1,1 +1,46 @@
 # implement histogan model
+import torch
+
+class ResidualBlock(torch.nn.Module):
+    def __init__(self, inp_dim, out_dim):
+        super().__init__()
+        ## residual block architecture is taken from the paper Figure S1
+        self.conv1 = torch.nn.Conv2d(inp_dim, out_dim, kernel_size=3, padding=1, stride=1)
+        self.conv2 = torch.nn.Conv2d(out_dim, out_dim, kernel_size=3, padding=1, stride=1)
+        self.conv1x1 = torch.nn.Conv2d(inp_dim, out_dim, kernel_size=1, padding=0, stride=1)
+        self.conv3 = torch.nn.Conv2d(out_dim, out_dim, kernel_size=3, padding=1, stride=2)
+        self.lrelu = torch.nn.LeakyReLU()
+
+    def forward(self, x):
+        out = self.lrelu(self.conv1(x))
+        out = self.lrelu(self.conv2(out))
+        out += self.conv1x1(x)
+        out = self.conv3(out)
+        return out
+
+class Discriminator(torch.nn.Module):
+    def __init__(self, num_res_block, output_channels):
+        super().__init__()
+        ## Discriminator architecture taken from Section 5.1. Details of Our Networks
+        residual_block_layers = []
+        in_dim = 3
+        for _ in range(num_res_block):
+            residual_block_layers.append(ResidualBlock(in_dim, output_channels))
+            in_dim = output_channels
+            output_channels = 2*output_channels 
+        self.residual_layers = torch.nn.Sequential(*residual_block_layers)
+
+        ## test tensor is used to calculate the input dimension to the fc layer
+        test =  torch.zeros((3,256,256))
+        with torch.no_grad():
+            out = self.residual_layers(test)
+        linear_inp = out.size(0) * out.size(1) * out.size(2)
+
+        self.fc = torch.nn.Linear(linear_inp, 1)
+    
+    def forward(self, x):
+        out = self.residual_layers(x)
+        out = torch.flatten(out, 1)
+        out = self.fc(out)
+        return out
+
