2a8de9b860a5dffe4abe004f4118c1763ea404d4
Burak Bolat <burakbolat_06@hotmail.com>
2022-05-26 18:30:12 +0300

    Minor update to losses.

--- loss.py
+++ loss.py
@@ -1,18 +1,19 @@
 # implement loss functions
+from dis import dis
 from matplotlib.pyplot import hist2d
 import utils
 import torch
 
 # See Eq. 5
-def loss(g):
-    d = disc_loss(g)
-    c = hellinger_dist_loss(g)
+# g: generated image, d_score: scalar output of discriminator
+def generator_loss(g, d_score):
+    c_loss = hellinger_dist_loss(g)
     alpha = 2.0  # See Sec. 5.2 Training details
-    total_loss = d + alpha*c
-    return total_loss 
+    g_loss = d_score + alpha*c_loss
+    return -g_loss 
 
-def disc_loss(g):
-    pass
+def disc_loss(g_scores, r_scores):
+    return torch.mean(-torch.log(r_scores)) - torch.mean(torch.log(1-g_scores))
 
 # This is color matching loss, see Eq. 4
 # It takes histogram of generated and target
@@ -40,10 +41,16 @@ def hellinger_dist_loss(g):
     h_norm = torch.norm(torch.norm(torch.norm(diff, dim=3), dim=2), dim=1)
     h_norm = h_norm * (torch.sqrt(torch.ones((g.shape[0]))*2))
     
+    # Used mean reduction, other option is sum reduction
+    h_norm = torch.mean(h_norm)
+
     return h_norm
 
 def main():
     g = torch.rand((2, 3, 64, 64))
     hellinger_dist_loss(g)
+    g_scores = torch.rand(15)
+    r_scores = torch.rand(19)
+    print(disc_loss(g_scores, r_scores))
 
 if __name__=="__main__": main()
