b8fba67ff1be63dff11c47b3752e3c1ea4fbe539
ozgraslan <ozgraslan17@gmail.com>
2022-05-26 18:21:25 +0300

    Implemented dataset and dataloader.

--- data.py
+++ data.py
@@ -1,1 +1,31 @@
 # implement torch.data.Dataset class
+from torch.utils.data import Dataset
+import os
+from torchvision.io import read_image
+from torchvision.transforms import Resize
+
+class AnimeFacesDataset(Dataset):
+    def __init__(self, image_dir, transform=Resize((256,256)), device="cpu"):
+        self.image_dir = image_dir
+        self.transform = transform
+        self.image_tensor_list = []
+        for root, dirs, files in os.walk(image_dir):
+            for file in files:
+                path = os.path.join(root, file)
+                image = read_image(path)
+                self.image_tensor_list.append(image.to(device))
+
+    def __len__(self):
+        return len(self.image_tensor_list)
+
+    def __getitem__(self, index):
+        return self.transform(self.image_tensor_list[index])
+
+
+def main():
+    image_dir = "images"
+    dataset = AnimeFacesDataset(image_dir, device="cuda")
+    print(dataset.size())
+
+if __name__ == "__main__":
+    main()
--- train.py
+++ train.py
@@ -1,1 +1,16 @@
 # implement training loop 
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+from data import AnimeFacesDataset
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+image_dir = "images"
+transfrom = transforms.Compose(
+    [transforms.Resize((256,256)),
+     transforms.RandomHorizontalFlip(0.5)])
+dataset = AnimeFacesDataset(image_dir, transfrom, device)
+
+batch_size = 2
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
