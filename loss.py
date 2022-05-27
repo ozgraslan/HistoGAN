Fixed some histogram computation based issues.
Tried different learning rates for stable training.
Currently training only the StyleGan2 model.

loss.py
@@ -7,13 +7,13 @@ import torch
 # See Eq. 5
 # g: generated image, d_score: scalar output of discriminator
 def generator_loss(g, d_score, hist_t):
-    c_loss = hellinger_dist_loss(g, hist_t)
-    alpha = 2.0  # See Sec. 5.2 Training details
-    g_loss = torch.mean(d_score) - alpha*c_loss
+    #c_loss = hellinger_dist_loss(g, hist_t)
+    #alpha = 2.0  # See Sec. 5.2 Training details
+    g_loss = torch.mean(torch.log(torch.sigmoid(d_score))) #- alpha*c_loss
     return -g_loss 
 
 def disc_loss(g_scores, r_scores):
-    return torch.mean(-torch.log(torch.sigmoid(r_scores))) - torch.mean(torch.log(1-torch.sigmoid(g_scores)))
+    return -torch.mean(torch.log(torch.sigmoid(r_scores))) - torch.mean(torch.log(1-torch.sigmoid(g_scores)))
 
 # This is color matching loss, see Eq. 4
 # It takes histogram of generated and target
@@ -23,7 +23,7 @@ def hellinger_dist_loss(g, hist_t):
     g_sqred = torch.sqrt(hist_g)
     diff = t_sqred - g_sqred
     h = torch.sum(torch.square(diff), dim=(1,2,3))
-    print(hist_t.min(), hist_g.min())
+    # print(hist_t.min(), hist_g.min())
     h_norm = torch.sqrt(h)
     h_norm = h_norm * (torch.sqrt(torch.ones((g.shape[0]))/2))
     

model.py
@@ -2,6 +2,8 @@
 from re import S
 import torch
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
 class ResidualBlock(torch.nn.Module):
     def __init__(self, inp_dim, out_dim):
         super().__init__()
@@ -123,8 +125,8 @@ class StyleGAN2Block(torch.nn.Module):
         # in style gan2 paper each noise is tought as a N(0,I) image 
         # with same height and with as the feature map
         batch, channel, height, width = fm.size()  
-        noise_1 = torch.randn((batch, 1, height, width))
-        noise_2 = torch.randn((batch, 1, height, width))
+        noise_1 = torch.randn((batch, 1, height, width)).to(device)
+        noise_2 = torch.randn((batch, 1, height, width)).to(device)
         style_1 = self.affine_1(w)
         style_2 = self.affine_2(w)
         style_3 = self.affine_3(w)
@@ -175,22 +177,22 @@ class HistoGAN(torch.nn.Module):
         B = z.size(0)
         w = self.latent_mapping(z)
         fm = self.learned_const_inp.unsqueeze(0).repeat(B, 1, 1, 1)
-        for i, stylegan2_block in enumerate(self.stylegan2_blocks[:-1]):
+        # for i, stylegan2_block in enumerate(self.stylegan2_blocks[:-1]):
+        for i, stylegan2_block in enumerate(self.stylegan2_blocks):
             fm, rgb = stylegan2_block(fm, w)
             fm = self.upsample(fm)
             if i == 0:
                 rgb = self.upsample(rgb)
                 rgb_sum = rgb
+            elif i == len(self.stylegan2_blocks) -1 :
+                rgb_sum += rgb
             else:
                 rgb_sum += rgb
                 rgb_sum = self.upsample(rgb_sum)
-        # print("hello")
-        # print(target_hist.size())
-        hist_w = self.hist_projection(target_hist.flatten(1))
-        # print(hist_w.size())
-        _, rgb = self.stylegan2_blocks[-1](fm, hist_w)
-        # print(rgb.size(), rgb_sum.size())
-        rgb_sum += rgb
-    
+
+        # hist_w = self.hist_projection(target_hist.flatten(1))
+        # _, rgb = self.stylegan2_blocks[-1](fm, hist_w)
+        # rgb_sum += rgb
+        # print("sum size:", rgb_sum.size())
         return rgb_sum
     

train.py
@@ -3,6 +3,7 @@ from pkg_resources import run_script
 import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms
+from torchvision.utils import save_image
 
 from torch_optimizer import DiffGrad
 from data import AnimeFacesDataset
@@ -10,24 +11,24 @@ from model import Discriminator, HistoGAN
 from loss import generator_loss, disc_loss
 from utils import histogram_feature_v2
 
+import os
 torch.autograd.set_detect_anomaly(True)
 def random_interpolate_hists(batch_data):
     B = batch_data.size(0)
-    delta = torch.rand((B,1,1,1)) 
-    first_images = torch.index_select(batch_data, dim=0, index=torch.randint(0, B, (B,)))
-    second_images = torch.index_select(batch_data, dim=0, index=torch.randint(0, B, (B,)))
+    delta = torch.rand((B,1,1,1)).to(device)
+    first_images = torch.index_select(batch_data, dim=0, index=torch.randint(0, B, (B,)).to(device))
+    second_images = torch.index_select(batch_data, dim=0, index=torch.randint(0, B, (B,)).to(device))
     first_hist = histogram_feature_v2(first_images)
     second_hist = histogram_feature_v2(second_images)
     hist_t = delta*first_hist + (1-delta)*second_hist
     return hist_t
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-device="cpu"  # Test for ram usage, histogram is computed on cpu, fix it
-image_dir = "images"
+real_image_dir = "images"
 transfrom = transforms.Compose(
     [transforms.Resize((256,256)),
      transforms.RandomHorizontalFlip(0.5)])
-dataset = AnimeFacesDataset(image_dir, transfrom, device)
+dataset = AnimeFacesDataset(real_image_dir, transfrom, device)
 
 batch_size = 4
 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
@@ -46,16 +47,23 @@ gene_optim = DiffGrad(
 )
 disc_optim = DiffGrad(
     discriminator.parameters(),
-    lr= 2e-4,
+    lr= 2e-5,
     betas=(0.9, 0.999),
     eps=1e-8,
     weight_decay=0,
 )
+fake_image_dir = "generated_images"
+if not os.path.isdir(fake_image_dir):
+    os.mkdir(fake_image_dir)
+save_epoch = 5
 for epoch in range(num_epochs):
     for iter, batch_data in enumerate(dataloader):
+        torch.cuda.empty_cache() 
+        print(100*iter*batch_data.size(0)/len(dataset))
+        batch_data = batch_data.to(device)
         disc_optim.zero_grad()
         # Sample random Gaussian noise
-        z = torch.rand(batch_data.size(0), 512)
+        z = torch.rand(batch_data.size(0), 512).to(device)
         # Interpolate between target image histogram 
         # to prevent overfitting to dataset images
         target_hist = random_interpolate_hists(batch_data)
@@ -83,7 +91,12 @@ for epoch in range(num_epochs):
             print("Gen loss:", loss.item())
             loss.backward()
             gene_optim.step()
-            
+    
+    if epoch % save_epoch == 0:
+        clamped_data = torch.clamp(fake_data, 0, 255)
+        for i in range(clamped_data.size(0)):
+            save_image(clamped_data[i], os.path.join(fake_image_dir, "fake_{}_{}.png".format(epoch, i)))
+        
 
 
 

utils.py
@@ -15,6 +15,7 @@ import gc
 # they are not mentioned in the paper.
 # Not mentioned in the paper but "sampling" is used to get img pixels of size h
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
 def histogram_feature(img, h=64, hist_boundary=[-3, 3], fall_off = 0.02):
     img_flat = torch.reshape(img, (img.shape[0], img.shape[1], -1))  # reshape such that img_flat = M,3,N*N
     
@@ -120,7 +121,7 @@ def get_hist_c(ur, u, vr, v, i_y, fall_off):
 
     return rdiffu
 
-def histogram_feature_v2(img, h=64, hist_boundary=[-3, 3], fall_off = 0.02, device="cuda"):
+def histogram_feature_v2(img, h=64, hist_boundary=[-3, 3], fall_off = 0.02):
     img = img / 255.0  # Map (0, 255) --> (0, 1)
     eps = 1e-6  # prevent taking log of 0 valued pixels
     img += eps  # Inplace
@@ -142,11 +143,11 @@ def histogram_feature_v2(img, h=64, hist_boundary=[-3, 3], fall_off = 0.02, devi
     ub = -vr
     vb = -vr + ur
 
-    u = torch.linspace(hist_boundary[0], hist_boundary[1], h)
+    u = torch.linspace(hist_boundary[0], hist_boundary[1], h).to(device)
     u = torch.unsqueeze(u, dim=0)   # Make (h,) to (1, h) so that 
                                     # for each element in ur there will 
                                     # be difference with each u value.
-    v = torch.linspace(hist_boundary[0], hist_boundary[1], h)
+    v = torch.linspace(hist_boundary[0], hist_boundary[1], h).to(device)
     v = torch.unsqueeze(v, dim=0)
 
     ur = torch.unsqueeze(ur, dim=2) # Make each element an array it 