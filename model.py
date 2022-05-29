Fixed some bugs in the training loop and the regularization loss computation.

model.py
@@ -189,10 +189,6 @@ class HistoGAN(torch.nn.Module):
             else:
                 rgb_sum += rgb
                 rgb_sum = self.upsample(rgb_sum)
-
-        # hist_w = self.hist_projection(target_hist.flatten(1))
-        # _, rgb = self.stylegan2_blocks[-1](fm, hist_w)
-        # rgb_sum += rgb
-        # print("sum size:", rgb_sum.size())
-        return rgb_sum
+        # w is returned to compute path lenght regularization
+        return rgb_sum, w 
     

train.py
@@ -1,6 +1,6 @@
 # implement training loop 
-from pkg_resources import run_script
 import torch
+import numpy as np
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.utils import save_image
@@ -12,63 +12,83 @@ from loss import generator_loss, disc_loss
 from utils import histogram_feature_v2
 
 import os
+
 torch.autograd.set_detect_anomaly(True)
-def random_interpolate_hists(batch_data):
-    B = batch_data.size(0)
-    delta = torch.rand((B,1,1,1)).to(device)
-    first_images = torch.index_select(batch_data, dim=0, index=torch.randint(0, B, (B,)).to(device))
-    second_images = torch.index_select(batch_data, dim=0, index=torch.randint(0, B, (B,)).to(device))
-    first_hist = histogram_feature_v2(first_images)
-    second_hist = histogram_feature_v2(second_images)
-    hist_t = delta*first_hist + (1-delta)*second_hist
-    return hist_t
+torch.backends.cudnn.benchmark = True
+
+# def random_interpolate_hists(batch_data):
+#     B = batch_data.size(0)
+#     delta = torch.rand((B,1,1,1)).to(device)
+#     first_images = torch.index_select(batch_data, dim=0, index=torch.randint(0, B, (B,)).to(device))
+#     second_images = torch.index_select(batch_data, dim=0, index=torch.randint(0, B, (B,)).to(device))
+#     first_hist = histogram_feature_v2(first_images)
+#     second_hist = histogram_feature_v2(second_images)
+#     hist_t = delta*first_hist + (1-delta)*second_hist
+#     return hist_t
+
+# def random_interpolate_hists(batch_data):
+#     hist_list = []
+#     hist = histogram_feature_v2(batch_data)
+#     hist_t = delta*first_hist + (1-delta)*second_hist
+#     return hist_t
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 real_image_dir = "images"
-transfrom = transforms.Compose(
+transform = transforms.Compose(
     [transforms.Resize((256,256)),
      transforms.RandomHorizontalFlip(0.5)])
-dataset = AnimeFacesDataset(real_image_dir, transfrom, device)
+dataset = AnimeFacesDataset(real_image_dir, transform, device)
 
-batch_size = 4
+batch_size = 2
 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 num_epochs = 100
 generator = HistoGAN().to(device)
 discriminator = Discriminator(7, 16).to(device)
 
 g_update_iter = 5
+acc_gradient_iter = 8
+r1_factor = 1
 
-gene_optim = DiffGrad(
+# please see StyleGAN2 paper B. Implementation Details Path length regularization
+ema_decay_coeff = 0.99
+target_scale = torch.tensor([0]).to(device)
+plr_factor = np.log(2)/(256**2*(np.log(256)-np.log(2)))
+
+save_iter = 200
+
+gene_optim = torch.optim.Adam(
     generator.parameters(),
     lr= 2e-4,
     betas=(0.9, 0.999),
     eps=1e-8,
     weight_decay=0,
 )
-disc_optim = DiffGrad(
+disc_optim = torch.optim.Adam(
     discriminator.parameters(),
     lr= 2e-5,
     betas=(0.9, 0.999),
     eps=1e-8,
     weight_decay=0,
 )
+
 fake_image_dir = "generated_images"
 if not os.path.isdir(fake_image_dir):
     os.mkdir(fake_image_dir)
-save_epoch = 5
+
 for epoch in range(num_epochs):
     for iter, batch_data in enumerate(dataloader):
         torch.cuda.empty_cache() 
-        print(100*iter*batch_data.size(0)/len(dataset))
+        training_percent = 100*iter*batch_data.size(0)/len(dataset)
         batch_data = batch_data.to(device)
-        disc_optim.zero_grad()
         # Sample random Gaussian noise
         z = torch.rand(batch_data.size(0), 512).to(device)
         # Interpolate between target image histogram 
         # to prevent overfitting to dataset images
-        target_hist = random_interpolate_hists(batch_data)
+        target_hist = None #random_interpolate_hists(batch_data)
         # Generate fake images
-        fake_data = generator(z, target_hist)
+        fake_data, w = generator(z, target_hist)
+        # fake_data = torch.clamp(fake_data, -256, 256)
+
         # Detach fake data so no gradient accumalition 
         # to generator while only training discriminator
         fake_data = fake_data.detach()
@@ -76,26 +96,57 @@ for epoch in range(num_epochs):
         # Compute real probabilities computed by discriminator
         fake_scores = discriminator(fake_data)
         real_scores = discriminator(batch_data)
-        # Compute discriminator loss
-        loss = disc_loss(fake_scores, real_scores)
-        print("Disc loss:", loss.item())
-        loss.backward()
-        disc_optim.step()
+        d_loss = disc_loss(fake_scores, real_scores)
 
-        if iter % g_update_iter == 0:
-            gene_optim.zero_grad()
-            fake_data = generator(z, target_hist) 
+        # in stylegan2 paper they argue applying regularization in every 16 iteration does not hurt perfrormance 
+        if (iter+1) % 16 == 0: 
+            # r1 regulatization
+            # for autograd.grad to work input should also have requires_grad = True
+            batch_data_grad = batch_data.clone().detach().requires_grad_(True)
+            real_score_for_r1 = discriminator(batch_data_grad)
+            gradients1 = torch.autograd.grad(outputs=real_score_for_r1, inputs=batch_data_grad, grad_outputs=torch.ones(real_score_for_r1.size()).to(device))[0]
+            r1_reg = torch.mean(torch.sum(torch.square(gradients1.view(gradients1.size(0), -1)), dim=1))
+            d_loss = d_loss + r1_factor*r1_reg  
+       
+        print("%", training_percent, " Disc loss:", d_loss.item())
+        d_loss.backward()
+
+        if ((iter + 1) % acc_gradient_iter == 0) or (iter + 1 == len(dataloader)):
+            disc_optim.step()
+            disc_optim.zero_grad()
+            print("disc updated")
+
+
+        if (iter+1) % g_update_iter == 0:
+            z = torch.rand(batch_data.size(0), 512).to(device)
+            fake_data, w = generator(z, target_hist) 
+            # fake_data = torch.clamp(fake_data, -256, 256)
+
             disc_score = discriminator(fake_data)
-            fake_data = torch.nn.functional.relu(fake_data)
-            loss = generator_loss(fake_data, disc_score, target_hist) 
-            print("Gen loss:", loss.item())
-            loss.backward()
-            gene_optim.step()
-    
-    if epoch % save_epoch == 0:
-        clamped_data = torch.clamp(fake_data, 0, 255)
-        for i in range(clamped_data.size(0)):
-            save_image(clamped_data[i], os.path.join(fake_image_dir, "fake_{}_{}.png".format(epoch, i)))
+            g_loss = generator_loss(fake_data, disc_score, target_hist) 
+
+            if (iter+1) % (8*g_update_iter) == 0:
+                gradients2 = torch.autograd.grad(outputs=fake_data*torch.randn_like(fake_data).to(device), inputs=w, grad_outputs=torch.ones(fake_data.size()).to(device), retain_graph=True)[0]
+                j_norm  = torch.sqrt(torch.sum(torch.square(gradients2.view(gradients2.size(0), -1)),dim=1))
+                plr = torch.mean(torch.square(j_norm - target_scale))
+                g_loss = g_loss + plr * plr_factor
+                target_scale = (1-ema_decay_coeff)* target_scale + ema_decay_coeff * j_norm
+
+            print("%", training_percent, "Gen loss:", g_loss.item())
+            g_loss.backward()
+
+            if ((iter + 1) % (g_update_iter*acc_gradient_iter) == 0) or (iter + 1 == len(dataloader)):
+                gene_optim.step()
+                gene_optim.zero_grad()
+                print("gene updated")
+
+
+            
+        if iter % save_iter == 0:
+            for i in range(fake_data.size(0)):
+                save_image(fake_data[i], os.path.join(fake_image_dir, "fake_{}_{}_{}.png".format(epoch, iter, i)))
+            torch.save(generator.state_dict(), "generator.pt")
+            
         
 
 