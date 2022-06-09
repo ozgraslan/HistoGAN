Fixed training loop error about accumulating gradient and changed some model blocks' implementation.

loss.py
@@ -59,6 +59,6 @@ def compute_gradient_penalty(fake_data, real_data, discriminator):
     comb_data = comb_data.requires_grad_(True)
     comb_score = discriminator(comb_data)
     gradients = torch.autograd.grad(outputs=comb_score, inputs=comb_data, grad_outputs=torch.ones(comb_score.size()).to(device), create_graph=True, retain_graph=True)[0]
-    gradient_norm = torch.sqrt(1e-6+torch.sum(torch.square(gradients.view(gradients.size(0), -1)), dim=1))
+    gradient_norm = torch.sqrt(1e-8+torch.sum(torch.square(gradients.view(gradients.size(0), -1)), dim=1))
     gradient_penalty = torch.mean(torch.square(gradient_norm-1))
     return gradient_penalty

model.py
@@ -1,17 +1,106 @@
 # implement histogan model
-from re import S
+from tkinter import W
 import torch
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
+
+class LinearLayer(torch.nn.Module):
+    def __init__(self, inp_size, out_size, lr_bias):
+        super().__init__()
+        self.weight = torch.nn.Parameter(torch.randn(inp_size, out_size))#.to(device)
+        self.bias = torch.nn.Parameter(torch.randn(out_size))#.to(device)
+        self.eqlr_coeff = lr_bias / torch.sqrt(torch.tensor([inp_size]))
+        self.eqlr_coeff = self.eqlr_coeff.to(device)
+
+    def forward(self, x):
+        weight = self.weight * self.eqlr_coeff 
+        z = torch.addmm(self.bias, x, weight)
+        return z
+
+def convolution(inp, weight, padding, stride):
+    # Implementaion of convolution operation using torch.unfold 
+    # Taken from https://pytorch.org/docs/stable/generated/torch.nn.Unfold.html
+    # Changed such that it can work with batch of styles
+    # After the operation the output feature maps' spatial size should be the same as the input, therefore padding=1
+    inp_unf = torch.nn.functional.unfold(inp, (weight.size(-1), weight.size(-1)), padding=(padding, padding), stride=(stride, stride)) 
+    if len(weight.size()) == 4:
+        out_unf = inp_unf.transpose(1, 2).matmul(weight.view(weight.size(0), -1).t()).transpose(1, 2)
+        outsize1 = int(1+(inp.size(2)+2*padding -weight.size(2))/stride) 
+        outsize2 = int(1+(inp.size(3)+2*padding -weight.size(3))/stride) 
+    else:
+        out_unf = inp_unf.transpose(1, 2).matmul(weight.view(weight.size(0), weight.size(1), -1).transpose(1,2)).transpose(1, 2)
+        outsize1 = int(1+(inp.size(2)+2*padding -weight.size(3))/stride) 
+        outsize2 = int(1+(inp.size(3)+2*padding -weight.size(4))/stride) 
+
+
+    out = torch.nn.functional.fold(out_unf, (outsize1, outsize2), (1, 1))
+    return out    
+
+class ModDemodConv3x3(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, padding=0, stride=1, moddemod=True):
+        super().__init__()
+        # StyleGAN papers mention each weight is initalized with N(0, I)
+        self.weight = torch.nn.Parameter(torch.randn(out_channels, in_channels, 3, 3))#.to(device)
+        self.padding = padding
+        self.stride = stride
+        self.moddemod = moddemod
+        if not self.moddemod:
+            self.bias = torch.nn.Parameter(torch.randn(out_channels))#.to(device)
+        else:
+            self.bias = None
+        
+        self.eqlr_coeff = 1/torch.sqrt(torch.tensor([in_channels*3*3]))
+        self.eqlr_coeff = self.eqlr_coeff.to(device)
+
+    def forward(self, x, style=None):
+        weight = self.weight * self.eqlr_coeff
+        if self.moddemod:
+            style = style.view(style.size(0),1,-1,1,1)
+            modulated_weight = style * weight
+            sigma = torch.sqrt(modulated_weight.square().sum(dim=(2,3,4), keepdim=True) +1e-8)
+            demodulated_weight = modulated_weight / sigma
+            out = convolution(x, demodulated_weight, self.padding, self.stride)
+        else:
+            out = convolution(x, weight, self.padding, self.stride) 
+            b,c,h,w = out.size()
+            out = out + torch.permute(self.bias.repeat(b,h,w,1), (0,3,1,2))
+        return out     
+
+class ModConv1x1(torch.nn.Module):
+    def __init__(self, in_channels, out_channels=3, padding=0, stride=1, mod=True):
+        super().__init__()
+        self.weight = torch.nn.Parameter(torch.randn(out_channels, in_channels, 1, 1))#.to(device)
+        self.padding = padding
+        self.stride = stride
+        self.mod = mod
+        if not self.mod:
+            self.bias = torch.nn.Parameter(torch.randn(out_channels))#.to(device) 
+        else:
+            self.bias = None
+
+        self.eqlr_coeff = 1/torch.sqrt(torch.tensor([in_channels]))
+        self.eqlr_coeff = self.eqlr_coeff.to(device)
+
+    def forward(self, x, style=None):
+        weight = self.weight * self.eqlr_coeff
+        if self.mod:
+            style = style.view(style.size(0),1,-1,1,1)
+            modulated_weight = style * weight
+            out = convolution(x, modulated_weight, self.padding, self.stride)
+        else:
+            out = convolution(x, weight, self.padding, self.stride) 
+            b,c,h,w = out.size()
+            out = out + torch.permute(self.bias.repeat(b,h,w,1), (0,3,1,2)) 
+        return out 
 
 class ResidualBlock(torch.nn.Module):
     def __init__(self, inp_dim, out_dim):
         super().__init__()
         ## residual block architecture is taken from the paper Figure S1
-        self.conv1 = torch.nn.Conv2d(inp_dim, out_dim, kernel_size=3, padding=1, stride=1)
-        self.conv2 = torch.nn.Conv2d(out_dim, out_dim, kernel_size=3, padding=1, stride=1)
-        self.conv1x1 = torch.nn.Conv2d(inp_dim, out_dim, kernel_size=1, padding=0, stride=1)
-        self.conv3 = torch.nn.Conv2d(out_dim, out_dim, kernel_size=3, padding=1, stride=2)
+        self.conv1 = ModDemodConv3x3(inp_dim, out_dim, padding=1, stride=1, moddemod=False)  # torch.nn.Conv2d(inp_dim, out_dim, kernel_size=3, padding=1, stride=1)
+        self.conv2 = ModDemodConv3x3(out_dim, out_dim, padding=1, stride=1, moddemod=False)  # torch.nn.Conv2d(out_dim, out_dim, kernel_size=3, padding=1, stride=1)
+        self.conv1x1 = ModConv1x1(inp_dim, out_dim, padding=0, stride=1, mod=False)          # torch.nn.Conv2d(inp_dim, out_dim, kernel_size=1, padding=0, stride=1)
+        self.conv3 = ModDemodConv3x3(out_dim, out_dim, padding=1, stride=2, moddemod=False)  # torch.nn.Conv2d(out_dim, out_dim, kernel_size=3, padding=1, stride=2)
         self.lrelu = torch.nn.LeakyReLU(0.2)
 
     def forward(self, x):
@@ -22,7 +111,7 @@ class ResidualBlock(torch.nn.Module):
         return out
 
 class Discriminator(torch.nn.Module):
-    def __init__(self, num_res_block=7, network_capacity=16):
+    def __init__(self, num_res_block=7, network_capacity=16, image_res=256):
         super().__init__()
         ## Discriminator architecture taken from Section 5.1. Details of Our Networks
         residual_block_layers = []
@@ -33,14 +122,14 @@ class Discriminator(torch.nn.Module):
             in_dim = output_channels
             output_channels = 2*output_channels 
         self.residual_layers = torch.nn.Sequential(*residual_block_layers)
-
         ## test tensor is used to calculate the input dimension to the fc layer
-        test =  torch.zeros((3,256,256))
         with torch.no_grad():
-            out = self.residual_layers(test)
-        linear_inp = out.size(0) * out.size(1) * out.size(2)
-
-        self.fc = torch.nn.Linear(linear_inp, 1)
+            residual_layers = self.residual_layers.to(device)
+            test =  torch.zeros((1, 3,image_res,image_res)).to(device)
+            out = residual_layers(test)
+        linear_inp = out.size(1) * out.size(2) * out.size(3)
+        self.fc = LinearLayer(linear_inp, 1, lr_bias=1)
+        # print(list(self.parameters()))
     
     def forward(self, x):
         out = self.residual_layers(x)
@@ -48,71 +137,24 @@ class Discriminator(torch.nn.Module):
         out = self.fc(out)
         return out
 
-class ModDemodConv3x3(torch.nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        # StyleGAN papers mention each weight is initalized with N(0, I)
-        self.weight = torch.nn.Parameter(torch.randn(out_channels, in_channels, 3, 3))
-    
-    def convolution(self, inp, weight):
-        # Implementaion of convolution operation using torch.unfold 
-        # Taken from https://pytorch.org/docs/stable/generated/torch.nn.Unfold.html
-        # Changed such that it can work with batch of styles
-        # After the operation the output feature maps' spatial size should be the same as the input, therefore padding=1
-        inp_unf = torch.nn.functional.unfold(inp, (3, 3), padding=(1,1)) 
-        # print(weight.view(weight.size(0), weight.size(1), -1).transpose(1, 2).size())
-        # print(inp_unf.transpose(1, 2).size())
-        out_unf = inp_unf.transpose(1, 2).matmul(weight.view(weight.size(0), weight.size(1), -1).transpose(1, 2)).transpose(1, 2)
-        out = torch.nn.functional.fold(out_unf, (inp.size(2), inp.size(3)), (1, 1))
-        return out    
-    
-    def forward(self, x, style):
-        style = style.view(style.size(0),1,-1,1,1)
-        modulated_weight = style * self.weight
-        sigma = torch.sqrt(modulated_weight.square().sum(dim=(2,3,4), keepdim=True) +1e-8)
-        # print(sigma.size())
-        demodulated_weight = modulated_weight / sigma
-        out = self.convolution(x, demodulated_weight)
-        return out     
-
-class ModConv1x1(torch.nn.Module):
-    def __init__(self, in_channels, out_channels=3):
-        super().__init__()
-        self.weight = torch.nn.Parameter(torch.randn(out_channels, in_channels, 1, 1))
-
-    def convolution(self, inp, weight):
-        # Implementaion of convolution operation using torch.unfold 
-        # Taken from https://pytorch.org/docs/stable/generated/torch.nn.Unfold.html
-        # Changed such that it can work with batch of styles
-        # After the operation the output feature maps' spatial size should be the same as the input, therefore padding=1
-        inp_unf = torch.nn.functional.unfold(inp, (1, 1)) 
-        out_unf = inp_unf.transpose(1, 2).matmul(weight.view(weight.size(0), weight.size(1), -1).transpose(1, 2)).transpose(1, 2)
-        out = torch.nn.functional.fold(out_unf, (inp.size(2), inp.size(3)), (1, 1))
-        return out    
-
-    def forward(self, x, style):
-        style = style.view(style.size(0),1,-1,1,1)
-        modulated_weight = style * self.weight
-        out = self.convolution(x, modulated_weight)
-        return out 
-
 class StyleGAN2Block(torch.nn.Module):
     # StyleGan2 Block depicted in Figure 2/A of HistoGAN paper
     def __init__(self, input_channel_size, output_channel_size): # these channel numbers should be decided soon
         super().__init__()
-        self.affine_1 = torch.nn.Linear(512, input_channel_size)
-        self.affine_2 = torch.nn.Linear(512, output_channel_size)
-        self.affine_3 = torch.nn.Linear(512, output_channel_size)
+        self.affine_1 = LinearLayer(512, input_channel_size, lr_bias=1)
+        self.affine_2 = LinearLayer(512, output_channel_size, lr_bias=1)
+        self.affine_3 = LinearLayer(512, output_channel_size, lr_bias=1)
         # In stylegan2 paper, section B. Implementation Details/Generator redesign  
         # authors say that they used only single shared scaling factor for each feature map 
         # and they initiazlize the factor by 0
         self.noise_scaling_factor_1 = torch.nn.Parameter(torch.zeros((1)))
         self.noise_scaling_factor_2 = torch.nn.Parameter(torch.zeros((1))) 
-        self.md3x3_1 = ModDemodConv3x3(input_channel_size, output_channel_size)
-        self.md3x3_2 = ModDemodConv3x3(output_channel_size, output_channel_size)
+        self.md3x3_1 = ModDemodConv3x3(input_channel_size, output_channel_size,padding=1)
+        self.md3x3_2 = ModDemodConv3x3(output_channel_size, output_channel_size, padding=1)
         self.m1x1 = ModConv1x1(output_channel_size, 3)
         self.bias_1 = torch.nn.Parameter(torch.zeros(output_channel_size))
         self.bias_2 = torch.nn.Parameter(torch.zeros(output_channel_size))
+
         self.lrelu = torch.nn.LeakyReLU(0.2)
 
 
@@ -130,6 +172,7 @@ class StyleGAN2Block(torch.nn.Module):
         style_2 = self.affine_2(w)
         style_3 = self.affine_3(w)
         # if bias is not reshaped gives error, this version can broadcast to batch
+        # print(self.md3x3_1(fm, style_1).size(), noise_1.size())
         out = self.md3x3_1(fm, style_1) + self.noise_scaling_factor_1 * noise_1 + self.bias_1.view(-1,1,1) 
         out = self.lrelu(out)
         out = self.md3x3_2(out, style_2) + self.noise_scaling_factor_2 * noise_2 + self.bias_2.view(-1,1,1)
@@ -138,30 +181,24 @@ class StyleGAN2Block(torch.nn.Module):
         return out, rgb_out
 
 class HistoGAN(torch.nn.Module):
-    def __init__(self, network_capacity=16, h=64, channel_sizes = [1024, 512, 512, 512, 256, 128, 64]):
+    def __init__(self, network_capacity=16, h=64, mapping_layer_num= 8, channel_sizes = [1024, 512, 512, 512, 256, 128, 64]):
         super().__init__()
         latent_mapping_list = []
-        latent_mapping_list.extend([torch.nn.Linear(512, 1024), 
-                                   torch.nn.LeakyReLU(0.2),
-                                   torch.nn.Linear(1024, 512),
-                                   torch.nn.LeakyReLU(0.2)])
-        hist_projection_list = []
-        hist_projection_list.extend([torch.nn.Linear(h*h*3, 1024), 
-                                    torch.nn.LeakyReLU(0.2),
-                                    torch.nn.Linear(1024, 512),
-                                    torch.nn.LeakyReLU(0.2)]) 
-        for i in range(6):
-            if i == 5:
-                latent_mapping_list.append(torch.nn.Linear(512, 512))
-                hist_projection_list.append(torch.nn.Linear(512, 512))
+        # hist_projection_list = []
+        # hist_projection_inp = h*h*3
+        for i in range(mapping_layer_num):
+            if i == (mapping_layer_num-1):
+                latent_mapping_list.append(LinearLayer(512, 512, 0.01))
+                #hist_projection_list.append(LinearLayer(512, 512, 0.01))
             else:
-                latent_mapping_list.extend([torch.nn.Linear(512, 512),
+                latent_mapping_list.extend([LinearLayer(512, 512, 0.01),
                                            torch.nn.LeakyReLU(0.2)])
-                hist_projection_list.extend([torch.nn.Linear(512, 512),
-                                            torch.nn.LeakyReLU(0.2)])
+
+                #hist_projection_list.extend([LinearLayer(hist_projection_inp, 512, 0.01), torch.nn.LeakyReLU(0.2)])
+                #hist_projection_inp  = 512
 
         self.latent_mapping = torch.nn.Sequential(*latent_mapping_list)
-        self.hist_projection = torch.nn.Sequential(*hist_projection_list)
+        #self.hist_projection = torch.nn.Sequential(*hist_projection_list)
         self.learned_const_inp = torch.nn.Parameter(torch.randn(4*network_capacity, 4, 4))
         self.upsample = torch.nn.Upsample(scale_factor=2, mode="bilinear")
         stylegan2_block_list = [] 
@@ -171,23 +208,50 @@ class HistoGAN(torch.nn.Module):
             inp_size = channel_size
         self.stylegan2_blocks = torch.nn.ModuleList(stylegan2_block_list)
 
+    def get_w_from_z(self, z):
+        with torch.no_grad():
+            w = self.latent_mapping(z)
+        return w
+
+    def gen_image_from_w(self, w, target_hist):
+        with torch.no_grad():
+            B = w.size(0)
+            fm = self.learned_const_inp.unsqueeze(0).repeat(B, 1, 1, 1)
+            for i, stylegan2_block in enumerate(self.stylegan2_blocks): # [:-1]
+                fm, rgb = stylegan2_block(fm, w)
+                fm = self.upsample(fm)
+                if i == 0:
+                    rgb = self.upsample(rgb)
+                    rgb_sum = rgb
+                elif i == len(self.stylegan2_blocks) -1:
+                    rgb_sum += rgb
+                else:
+                    rgb_sum += rgb
+                    rgb_sum = self.upsample(rgb_sum)
+            # hist_w  = self.hist_projection(target_hist.flatten(1))
+            # _, rgb = self.stylegan2_blocks[-1](fm, hist_w)
+            # rgb_sum += rgb
+        return rgb_sum
+
     def forward(self, z, target_hist):
         # noise input z, size: B, 512
         B = z.size(0)
         w = self.latent_mapping(z)
         fm = self.learned_const_inp.unsqueeze(0).repeat(B, 1, 1, 1)
-        for i, stylegan2_block in enumerate(self.stylegan2_blocks[:-1]):
+        for i, stylegan2_block in enumerate(self.stylegan2_blocks): # [:-1]
             fm, rgb = stylegan2_block(fm, w)
             fm = self.upsample(fm)
             if i == 0:
                 rgb = self.upsample(rgb)
                 rgb_sum = rgb
+            elif i == len(self.stylegan2_blocks)-1:
+                rgb_sum += rgb
             else:
                 rgb_sum += rgb
                 rgb_sum = self.upsample(rgb_sum)
-        hist_w  = self.hist_projection(target_hist.flatten(1))
-        _, rgb = self.stylegan2_blocks[-1](fm, hist_w)
-        rgb_sum += rgb
+        #hist_w  = self.hist_projection(target_hist.flatten(1))
+        #_, rgb = self.stylegan2_blocks[-1](fm, hist_w)
+        #rgb_sum += rgb
         # w is returned to compute path length regularization
         return rgb_sum, w 
     

train.py
@@ -4,7 +4,7 @@ import numpy as np
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.utils import save_image
-
+from torch_optimizer import DiffGrad
 from data import AnimeFacesDataset
 from model import Discriminator, HistoGAN
 from loss import compute_gradient_penalty, pl_reg, r1_reg, wgan_gp_disc_loss, wgan_gp_gen_loss
@@ -21,26 +21,26 @@ torch.backends.cudnn.benchmark = True
 device = "cuda" if torch.cuda.is_available() else "cpu"
 real_image_dir = "images"
 transform = transforms.Compose(
-        [transforms.Resize((256,256)),
+        [transforms.Resize((64,64)),
         transforms.RandomHorizontalFlip(0.5)])
-dataset = AnimeFacesDataset(real_image_dir, transform, device)
 # due to hardware limitations similar to paper's authors we kept the batch size small
 batch_size = 2
 # the dataset contains 63,632 datum, and the we could not make the network to generate meaningfull images therefore kept the epochs small and experimented
 num_epochs = 2
-# variable to hold after how many discriminator updates to update the generator
-g_update_iter = 5
 # after how many gradient accumulation to optimize parameters
-acc_gradient_iter = 1
+acc_gradient_total = 32
+acc_gradient_iter = acc_gradient_total //batch_size
 # scalar of R1 regularization
 r1_factor = 10
+r1_update_iter = 16
 # variables for Path length regularization
 # please see StyleGAN2 paper B. Implementation Details Path length regularization
 ema_decay_coeff = 0.99
 target_scale = torch.tensor([0]).to(device)
 plr_factor = np.log(2)/(256**2*(np.log(256)-np.log(2)))
+plr_update_iter = 16
 # after how many iterations to save the nework parameters and generated images
-save_iter = 200
+save_iter = 20
 # path to save generated images
 fake_image_dir = "generated_images"
 if not os.path.isdir(fake_image_dir):
@@ -52,86 +52,119 @@ network_capacity = 16
 # histogram's bin size
 bin_size = 64
 # the number of channels are decides as log2(image_res) -1 since we generate 256 res images, there are 7 channels
-generator_channel_sizes = [1024, 512, 512, 512, 256, 128, 64]
+generator_channel_sizes = [512, 512, 256, 128, 64] # for 64 res images 5 channels
 learning_rate = 2e-4
 # coefficient of gradient penalty
 coeff_penalty = 10 # same as the StyleGAN2 paper
-
+# number of layers for mapping z to w and hist to w_hist
+mapping_layer_num = 8
+# image resolution to generate
+image_res = 64
 # Initialize Dataset, Dataloader, Discriminator and Generator
 dataset = AnimeFacesDataset(real_image_dir, transform, device)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-generator = HistoGAN(network_capacity, bin_size, generator_channel_sizes)
-discriminator = Discriminator(num_res_blocks, network_capacity)
+dataloader = DataLoader(dataset, batch_size=acc_gradient_total, shuffle=True, drop_last=True)
+generator = HistoGAN(network_capacity, bin_size, mapping_layer_num, generator_channel_sizes)
+discriminator = Discriminator(num_res_blocks, network_capacity, image_res)
 
 # If a pretrained network exists, load their parameters to continue training
-if os.path.exists("generator.pt"):
-    generator.load_state_dict(torch.load("generator.pt"))
-if os.path.exists("discriminator.pt"):
-    discriminator.load_state_dict(torch.load("discriminator.pt"))
+# if os.path.exists("generator_new.pt"):
+#     generator.load_state_dict(torch.load("generator_new.pt"))
+# if os.path.exists("discriminator_new.pt"):
+#     discriminator.load_state_dict(torch.load("discriminator_new.pt"))
 
 
 discriminator = discriminator.to(device)
 generator=generator.to(device)
 
 # Initialize optimizers 
-gene_optim = torch.optim.Adam(generator.parameters(), lr= learning_rate)
-disc_optim = torch.optim.Adam(discriminator.parameters(), lr= learning_rate)
+gene_optim = DiffGrad(generator.parameters(), lr=learning_rate)
+disc_optim = DiffGrad(discriminator.parameters(), lr=learning_rate)
+
+
+def truncation_trick(generator, latent_size, batch_size): # target_hist
+    z = torch.randn((1000, latent_size)).to(device)
+    w = generator.get_w_from_z(z)
+    w_mean = torch.mean(w, dim=0, keepdim=True)
+    fake_imgs = generator.gen_image_from_w(w_mean.repeat(batch_size,1), None) # target_hist.size(0), target_hist
+    return fake_imgs
+
+def train_generator(generator, discriminator, gene_optim, batch_size, iter): # hist_list
+    global ema_decay_coeff
+    total_gene_loss = 0
+    gene_optim.zero_grad()
+    # for target_hist in hist_list:
+    for _ in range(acc_gradient_iter):
+        z = torch.randn(batch_size, 512).to(device) # target_hist.size(0)
+        fake_data, w = generator(z, None) # target_hist
+        disc_score = discriminator(fake_data)
+        g_loss = torch.mean(torch.nn.functional.softplus(-disc_score)) / acc_gradient_iter
+        if (iter+1) % plr_update_iter == 0:
+            plr, ema_decay_coeff = pl_reg(fake_data, w, target_scale, plr_factor, ema_decay_coeff)
+            g_loss = g_loss + (plr / acc_gradient_iter) 
+        g_loss.backward()
+        total_gene_loss += g_loss.item()
+
+    print("Gen loss:", total_gene_loss)
+    gene_optim.step()
+    gene_optim.zero_grad()
+    # print("gene updated")
+
+def train_discriminator(generator, discriminator, disc_optim, chunk_data, batch_size, iter):
+    #hist_list = []
+    disc_optim.zero_grad()
+    total_real_loss = 0
+    total_fake_loss = 0
+    # total_gp_loss = 0
+    for index in range(chunk_data.size(0)//batch_size):
+        batch_data = chunk_data[index*batch_size:(index+1)*batch_size]
+        # batch_data = batch_data.to(device)
+        #target_hist = random_interpolate_hists(batch_data)
+        #hist_list.append(target_hist.clone())
+        z = torch.randn(batch_size, 512).to(device)
+        fake_data, _ = generator(z, None) #target_hist
+        fake_data = fake_data.detach()
+        fake_scores = discriminator(fake_data)
+        real_scores = discriminator(batch_data)
+        # gradient_penalty = compute_gradient_penalty(fake_data, batch_data, discriminator)
+        real_loss = torch.mean(torch.nn.functional.softplus(-real_scores)) / acc_gradient_iter
+        if (iter+1) % r1_update_iter == 0:
+            r1_loss =  r1_reg(batch_data, discriminator, r1_factor) / acc_gradient_iter
+            real_loss = real_loss + r1_loss
+        fake_loss = torch.mean(torch.nn.functional.softplus(fake_scores)) / acc_gradient_iter
+        # gp_loss = coeff_penalty * gradient_penalty / acc_gradient_iter
+        real_loss.backward()
+        fake_loss.backward()
+        # gp_loss.backward()
+        total_fake_loss += fake_loss.item()
+        total_real_loss += real_loss.item()
+        # total_gp_loss += gp_loss.item()
+
+    print("Real loss:", total_real_loss, "Fake loss:", total_fake_loss) # , "GP loss:", total_gp_loss
+    disc_optim.step()
+    disc_optim.zero_grad()
+    # print("disc updated")
+    #return hist_list
+        
+
 
 # Traning loop without gradient accumulation
 # Gradient accumulation is implemented and tried in train2.py but had some performance and memory consumption issues therefore not added here
+dataset_size = len(dataset)
 for epoch in range(num_epochs):
-    for iter, batch_data in enumerate(dataloader):
-        # torch.cuda.empty_cache() 
-        training_percent = 100*iter*batch_data.size(0)/len(dataset)
-        batch_data = batch_data.to(device)
-        # Sample random Gaussian noise
-        z = torch.randn(batch_data.size(0), 512).to(device)
-        # Interpolate between target image histogram 
-        # to prevent overfitting to dataset images
-        target_hist = random_interpolate_hists(batch_data)
-        # Generate fake images
-        fake_data, w = generator(z, target_hist)
+    for iter, chunk_data in enumerate(dataloader):
+        # print(batch_data.max(), batch_data.min())
+        training_percent = 100*iter*chunk_data.size(0)/dataset_size
+        print("Training %", training_percent)
+        train_discriminator(generator, discriminator, disc_optim, chunk_data, batch_size, iter) # hist_list = 
+        train_generator(generator, discriminator, gene_optim, batch_size, iter)
 
-        # Detach fake data so no gradient accumalition 
-        # to generator while only training discriminator
-        fake_data = fake_data.detach()
-
-        # Compute real probabilities computed by discriminator
-        fake_scores = discriminator(fake_data)
-        real_scores = discriminator(batch_data)
-        gradient_penalty = compute_gradient_penalty(fake_data, batch_data, discriminator)
-        d_loss = wgan_gp_disc_loss(real_scores, fake_scores, gradient_penalty, coeff_penalty)
-        #d_loss = disc_loss(fake_scores, real_scores)
-        # in stylegan2 paper they argue applying regularization in every 16 iteration does not hurt perfrormance 
-        if (iter+1) % 16 == 0: 
-            # r1 regulatization
-            d_loss = d_loss + r1_reg(batch_data, discriminator, r1_factor)  
-
-        print("%", training_percent, " Disc loss:", d_loss.item())
-        d_loss.backward()
-        disc_optim.step()
-        disc_optim.zero_grad()
-
-        if (iter+1) % g_update_iter == 0:
-            z = torch.randn(batch_data.size(0), 512).to(device)
-            fake_data, w = generator(z, target_hist) 
-
-            disc_score = discriminator(fake_data)
-            g_loss = wgan_gp_gen_loss(disc_score)
-            if (iter+1) % (8*g_update_iter) == 0:
-                plr, ema_decay_coeff = pl_reg(fake_data, w, target_scale, plr_factor, ema_decay_coeff)
-                g_loss = g_loss + plr
-
-            print("%", training_percent, "Gen loss:", g_loss.item())
-            g_loss.backward()
-            gene_optim.step()
-            gene_optim.zero_grad()
             
         if (iter+1) % save_iter == 0:
+            fake_data = torch.clamp(truncation_trick(generator, 512, batch_size), 0, 1) # hist_list[-1]
             for i in range(fake_data.size(0)):
                 save_image(fake_data[i], os.path.join(fake_image_dir, "fake_{}_{}_{}.png".format(epoch, iter, i)))
-            torch.save(generator.state_dict(), "generator.pt")
-            torch.save(discriminator.state_dict(), "discriminator.pt")
+            torch.save(generator.state_dict(), "generator_new.pt")
+            torch.save(discriminator.state_dict(), "discriminator_new.pt")
         
 
 